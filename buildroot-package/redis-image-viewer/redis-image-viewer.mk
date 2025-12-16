30e6786abd7c3c7d8bb6aab10f215ceb10f30f27
ilian <i.stoyanov@jpl.bg>
2025-12-17 00:15:21 +0200

    updated with buildroot-task dir

--- buildroot-package/redis-image-viewer/redis-image-viewer.mk
+++ buildroot-package/redis-image-viewer/redis-image-viewer.mk
@@ -3,7 +3,7 @@
 ###############################################################################
 
 REDIS_IMAGE_VIEWER_VERSION = 1.0
-REDIS_IMAGE_VIEWER_SITE = $(TOPDIR)/../br-task
+REDIS_IMAGE_VIEWER_SITE = $(TOPDIR)/../buildroot-task
 REDIS_IMAGE_VIEWER_SITE_METHOD = local
 REDIS_IMAGE_VIEWER_LICENSE = unknown
 REDIS_IMAGE_VIEWER_DEPENDENCIES = sdl2 sdl2_image hiredis
@@ -13,12 +13,12 @@ REDIS_IMAGE_VIEWER_SUPPORTS_IN_SOURCE_BUILD = NO
 REDIS_IMAGE_VIEWER_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 define REDIS_IMAGE_VIEWER_INSTALL_TARGET_CMDS
-	$(cmake-package-install-target)
+	$(MAKE) -C $(@D)/buildroot-build DESTDIR=$(TARGET_DIR) install
 	$(INSTALL) -D -m 0644 $(@D)/app.cfg.json $(TARGET_DIR)/etc/redis-image-viewer/config.json
-	$(INSTALL) -d $(TARGET_DIR)/var/lib/redis-image-viewer/images
 	$(INSTALL) -D -m 0644 $(REDIS_IMAGE_VIEWER_PKGDIR)/redis-image-viewer.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/redis-image-viewer.service
-	ln -sf ../../../../../usr/lib/systemd/system/redis-image-viewer.service \
+	$(INSTALL) -d $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -sf ../../usr/lib/systemd/system/redis-image-viewer.service \
 		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/redis-image-viewer.service
 endef
 
